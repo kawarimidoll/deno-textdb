d27e5e1baac2c3307a62c79963b9cdc44dc86f69
kawarimidoll <kawarimidoll+git@gmail.com>
2021-08-16 21:28:00 +0900

    test: add textdb_test.ts

---
+++ textdb_test.ts
@@ -0,0 +1,33 @@
+import {
+  assert,
+  assertEquals,
+} from "https://deno.land/std@0.104.0/testing/asserts.ts";
+import { TextDB } from "./textdb.ts";
+
+// test page id
+const TEXTDB_ENDPOINT = "247213c6-8bb7-4576-a89f-e72584046c71";
+
+Deno.test("TextDB", async () => {
+  const db = new TextDB(TEXTDB_ENDPOINT);
+  assert(db);
+
+  await db.clear();
+  assertEquals(await db.get(), "");
+
+  await db.put("this is text db!");
+  assertEquals(await db.get(), "this is text db!");
+
+  await db.put(`
+    this is multiline input!
+    this is multiline input!
+    this is multiline input!
+  `.trim());
+  assertEquals(
+    await db.get(),
+    `
+    this is multiline input!
+    this is multiline input!
+    this is multiline input!
+  `.trim(),
+  );
+});

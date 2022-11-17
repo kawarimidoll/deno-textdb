aa46c604e5bd4150583fa8a63a53e01617ba50a3
GitHub <noreply@github.com>
2022-11-17 01:14:10 +0000

    chore(deps): update deno dependencies

--- deps.ts
+++ deps.ts
@@ -4,5 +4,5 @@ export {
   assertEquals,
   assertObjectMatch,
   assertRejects,
-} from "https://deno.land/std@0.163.0/testing/asserts.ts";
-export { validate as validateUUID } from "https://deno.land/std@0.163.0/uuid/v4.ts";
+} from "https://deno.land/std@0.165.0/testing/asserts.ts";
+export { validate as validateUUID } from "https://deno.land/std@0.165.0/uuid/v4.ts";
